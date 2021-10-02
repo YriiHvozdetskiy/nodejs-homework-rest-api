7c8e867a35dacb226b1adcb9906cf07db36837f6
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-10-02 13:18:24 +0300

    finish

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,9 @@
 const validation = require('./validation')
 const controllerWrapper = require('./controllerWrapper')
+const validationStatusContact = require('./validationStatusContact')
 
 module.exports = {
   validation,
   controllerWrapper,
+  validationStatusContact,
 }
---
+++ models/index.js
@@ -0,0 +1,9 @@
+const { Contact } = require('./contact')
+const { joiSchema } = require('./contact')
+const { joiSchemaStatusContact } = require('./contact')
+
+module.exports = {
+  Contact,
+  joiSchema,
+  joiSchemaStatusContact,
+}
